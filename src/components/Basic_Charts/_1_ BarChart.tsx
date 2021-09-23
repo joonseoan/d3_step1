7014daf854aafdc8148cdb44f11d96b288d39be3
joonseoan <joonseo.an@outlook.com>
2021-09-22 22:18:50 -0400

    added colors and label

--- src/components/Basic_Charts/_1_ BarChart.tsx
+++ src/components/Basic_Charts/_1_ BarChart.tsx
@@ -4,9 +4,17 @@ import { select } from "d3";
 const BarChart = () => {
   const barRef = useRef<HTMLDivElement | null>(null);
   const w = 300;
-  const h = 100;
+  // const h = 100;
+  const h = 120;
   const padding = 2;
-  const dataset = [5, 10, 14, 20, 25];
+  const dataset = [5, 10, 14, 20, 25, 11, 25, 22, 18];
+  const colorPicker = (data: number) => {
+    if (data <= 20) {
+      return "#666666";
+    }
+
+    return "#FF0333";
+  };
 
   useEffect(() => {
     const svg = select(barRef.current)
@@ -19,12 +27,38 @@ const BarChart = () => {
       .data(dataset)
       .enter()
       .append("rect")
+
+      // [Adding colors]
       .attr("x", (d, i) => {
         return (i * w) / dataset.length;
       })
       .attr("y", (d) => h - d * 4)
       .attr("width", w / dataset.length - padding)
-      .attr("height", (d) => d * 4);
+      .attr("height", (d) => d * 4)
+
+      // 2) specifying color in terms of the data
+      .attr("fill", (d) => colorPicker(d));
+
+    // 1) Different colors
+    // adding colors4
+    // .attr("fill", (d) => `rgb(${d * 10}, 0, 0)`);
+
+    // [Adding Labels]
+    svg
+      .selectAll("text")
+      .data(dataset)
+      .enter()
+      .append("text")
+      .text((d) => d)
+      .attr("text-anchor", "middle")
+      .attr(
+        "x",
+        (d, i) => i * (w / dataset.length) + (w / dataset.length - padding) / 2
+      )
+      .attr("y", (d) => h - d * 4 + 14) // 100 --->  120
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 12)
+      .attr("fill", "white");
   }, []);
 
   return (

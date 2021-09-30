f7287c4992761371f5b88fa80c062153e9929485
Joon Seo An <joonseo.an@outlook.com>
2021-09-29 23:42:55 -0400

    fixed typescript issue

--- src/components/ExternalDataSource/CSV.tsx
+++ src/components/ExternalDataSource/CSV.tsx
@@ -1,23 +1,54 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import * as d3 from "d3";
 
+interface MonthlyData {
+  month: number;
+  sales: number;
+}
+
 const CSV = () => {
+  const [data, setData] = useState<MonthlyData[] | []>([]);
   const ref = useRef<HTMLDivElement | null>(null);
   const h = 100;
   const w = 400;
-  // let ds;
+
+  const lineFunc = d3
+    .line<MonthlyData>()
+    .x((d) => d.month * 2013)
+    .y((d) => h - d.sales)
+    .curve(d3.curveLinear);
 
   useEffect(() => {
-    console.log("ddddddkkkkkkkk");
-    d3.csv("../../data/MonthlySales.csv", (error: any, data: any) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("data: ", data);
-      }
-      return data;
-    });
-  }, []);
+    if (!data.length) {
+      const getCSVdata = async () => {
+        const ds = (await d3.csv("./data/monthlySales.csv"))
+          .slice(0, 12)
+          .map(({ month, sales }) => {
+            return {
+              month: month ? +month : 0,
+              sales: sales ? +sales : 0,
+            };
+          });
+
+        setData([...ds]);
+      };
+      getCSVdata();
+    }
+
+    console.log("data: ", data);
+
+    const svg = d3
+      .select(ref.current)
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+
+    svg
+      .append("path")
+      .attr("d", lineFunc(data))
+      .attr("stroke", "purple")
+      .attr("fill", "none");
+  }, [data]);
 
   return <div ref={ref} />;
 };

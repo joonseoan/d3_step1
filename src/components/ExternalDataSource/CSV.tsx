0d43dba93d02ec74d4daf6df21fcca4ab8e9b5cb
Joon Seo An <joonseo.an@outlook.com>
2021-09-30 22:46:00 -0400

    done for csv file

--- src/components/ExternalDataSource/CSV.tsx
+++ src/components/ExternalDataSource/CSV.tsx
@@ -14,43 +14,67 @@ const CSV = () => {
 
   const lineFunc = d3
     .line<MonthlyData>()
-    .x((d) => d.month * 2013)
+    .x((d) => (d.month - 20130001) / 3.25)
     .y((d) => h - d.sales)
     .curve(d3.curveLinear);
 
   useEffect(() => {
-    if (!data.length) {
-      const getCSVdata = async () => {
-        const ds = (await d3.csv("./data/monthlySales.csv"))
-          .slice(0, 12)
-          .map(({ month, sales }) => {
-            return {
-              month: month ? +month : 0,
-              sales: sales ? +sales : 0,
-            };
-          });
+    (async () => {
+      const ds = (await d3.csv("./data/monthlySales.csv"))
+        .slice(0, 12)
+        .map(({ month, sales }) => {
+          return {
+            month: month ? +month : 0,
+            sales: sales ? +sales : 0,
+          };
+        });
 
-        setData([...ds]);
-      };
-      getCSVdata();
-    }
+      const svg = d3
+        .select(ref.current)
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
 
-    console.log("data: ", data);
+      svg
+        .append("path")
+        .attr("d", lineFunc(ds))
+        .attr("stroke", "purple")
+        .attr("stroke-width", 2)
+        .attr("fill", "none");
 
-    const svg = d3
-      .select(ref.current)
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
+      // [Important: adding table in the chart
+      const t = d3.select(ref.current).append("table");
 
-    svg
-      .append("path")
-      .attr("d", lineFunc(data))
-      .attr("stroke", "purple")
-      .attr("fill", "none");
-  }, [data]);
+      let salesTotal = 0;
+      const metrics = [];
 
-  return <div ref={ref} />;
+      for (let i = 0; i < ds.length; i++) {
+        salesTotal += +ds[i].sales;
+      }
+
+      const average = salesTotal / ds.length;
+
+      metrics.push("Sales total: " + salesTotal);
+      metrics.push("Average: " + average.toFixed(2));
+
+      console.log("metrics: ", metrics);
+      t.selectAll("tr")
+        .data(metrics)
+        .enter()
+        // two tr and two td because of the length of metrics.
+        .append("tr")
+        .append("td")
+        .text((d) => d);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h1>CSV File</h1>
+      <div ref={ref} />
+    </div>
+  );
+  // return <div ref={ref} />;
 };
 
 export default CSV;
